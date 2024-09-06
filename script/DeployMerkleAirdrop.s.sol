6c310ffc9edaf76dff312025b13b1eecca1cd45b
zhouhangmyers <zhouhangmyers@gmail.com>
2024-09-06 22:27:50 +0800

    update

--- script/DeployMerkleAirdrop.s.sol
+++ script/DeployMerkleAirdrop.s.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import { MerkleAirdrop, IERC20 } from "../src/MerkleAirdrop.sol";
-import { Script } from "forge-std/Script.sol";
-import { BagelToken } from "../src/BagelToken.sol";
-import { console } from "forge-std/console.sol";
+import {MerkleAirdrop, IERC20} from "../src/MerkleAirdrop.sol";
+import {Script} from "forge-std/Script.sol";
+import {BagelToken} from "../src/BagelToken.sol";
+import {console} from "forge-std/console.sol";
 
 contract DeployMerkleAirdrop is Script {
     bytes32 public ROOT = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
